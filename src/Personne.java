5bbf91b087060c3b0858d14871ab23c52a85dba7
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2025-10-07 16:32:39 +0200

    Update Personne.java

--- src/Personne.java
+++ src/Personne.java
@@ -2,8 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package projethopitalvehicule;
-
 /**
  *
  * @author Chetouani
@@ -55,3 +53,4 @@ public class Personne {
     
     
 }
+

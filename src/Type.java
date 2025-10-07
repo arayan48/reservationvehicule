3604286c7ddbcc0ed3c4dccea32c7e53bf691894
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2025-10-07 16:32:54 +0200

    Update Type.java

--- src/Type.java
+++ src/Type.java
@@ -2,8 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package projethopitalvehicule;
-
 /**
  *
  * @author Chetouani
@@ -42,3 +40,4 @@ public class Type {
     
     
 }
+

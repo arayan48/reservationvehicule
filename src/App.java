48a704616522ef8dd63e23a7d30e14ba26e21386
arayan48 <arabirayanma@gmail.com>
2025-10-07 15:59:00 +0200

    Add PostgreSQL connection handling and update App class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.sql.Connection;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        connection connection = new connection();
     }
 }
--- src/connection.java
+++ src/connection.java
@@ -1,3 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class connection {
-    String url = "jdbc"
+    String url = "jdbc:postgresql://192.168.1.46:5432/slam_reservation_vehicule";
+    String user = "rayan";
+    String passwd = "Rayan789";
+
+    public connection() {
+        try {
+            Connection conn = DriverManager.getConnection(url, user, passwd);
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Connexion effective !");
+            } else {
+                System.out.println("Échec de la connexion !");
+            }
+        } catch (Exception e) {
+            System.out.println("Erreur de connexion : " + e.getMessage());
+        }
+    }
 }

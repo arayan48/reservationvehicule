6a43d79085f0d7328d005b15d381a89de8695f8d
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2025-10-14 12:59:39 +0200

    Squelette de la classe Passerelle

---
+++ src/Passerelle.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projethopitalvehicule;
+
+/**
+ *
+ * @author Chetouani
+ */
+public class Passerelle {
+    
+    public void demande (){
+        
+    }
+    
+    public void validation (){
+        
+    }
+    
+    public void modification (){
+        
+    }
+}
